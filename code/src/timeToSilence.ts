4b8bf3355d1d55af26540eef7b5403326c194098
Daniel <daniel.brownell@gmail.com>
2026-01-08 10:41:24 +0200

    Delete code directory

--- code/src/timeToSilence.ts
+++
@@ -1,9 +0,0 @@
-export function computeTimeToSilence(spikeMasses: number[], silentSpikeThreshold: number, tTrans: number): number {
-  const maxT = Math.min(tTrans, spikeMasses.length);
-  for (let t = 0; t < maxT; t++) {
-    if (spikeMasses[t] <= silentSpikeThreshold) {
-      return t;
-    }
-  }
-  return tTrans;
-}
